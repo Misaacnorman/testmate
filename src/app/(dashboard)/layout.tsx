97e3c571e20d2c1a993d6d41a0543b247ac0814a
Mwine Isaac Norman <MwineIsaacNorman@gmail.com>
2025-07-25 20:11:16 +0000

    delete the moduleregister sample

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ import {
   SidebarMenuButton,
   SidebarInset,
 } from "@/components/ui/sidebar";
-import { Beaker, TestTubeDiagonal, FilePlus2 } from "lucide-react";
+import { Beaker, TestTubeDiagonal } from "lucide-react";
 import Link from 'next/link';
 
 export default function DashboardLayout({
@@ -32,14 +32,6 @@ export default function DashboardLayout({
                 <Link href="/tests">
                   <TestTubeDiagonal />
                   <span>Tests</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <Link href="/register-sample">
-                  <FilePlus2 />
-                  <span>Register Sample</span>
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
