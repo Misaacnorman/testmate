8686bf9e66ef7a7477b91a6aeaf68a2257f8645c
Mwine Isaac Norman <MwineIsaacNorman@gmail.com>
2025-09-08 23:58:35 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'fi

--- .next/static/chunks/src_app_admin_page_tsx_91c7e7bf._.js
+++ .next/static/chunks/src_app_admin_page_tsx_91c7e7bf._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_0b42ad51._.js",
-  "static/chunks/node_modules_258c8221._.js"
+  "static/chunks/src_2ec92616._.js",
+  "static/chunks/node_modules_2a175b23._.js"
 ],
     source: "dynamic"
 });
